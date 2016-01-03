f25e517fadaabafbbb081ef4742e48ec4632c6bd
Isis Lovecruft <isis@torproject.org>
2016-01-03 16:43:46 +0000

    Try using bingen! macro to generate bindings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
-#[test]
-fn it_works() {
+
+#![feature(phase)]
+#[phase(plugin)] extern crate bindgen;
+
+#[allow(dead_code, uppercase_variables, non_camel_case_types)]
+mod nfc_bindings {
+    bindgen!("/usr/include/nfc/nfc.h",
+             "/usr/include/nfc/nfc-types.h",
+             "/usr/include/nfc/nfc-emulation.h",
+             match="nfc*.h", link="nfc")
 }
