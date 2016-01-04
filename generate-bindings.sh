fa76865b7f5691eb6c81db7cdd1dffa1861df29a
Isis Lovecruft <isis@torproject.org>
2016-01-04 22:41:30 +0000

    Just use includes with nfc.h to reduce code duplication.

--- generate-bindings.sh
+++ generate-bindings.sh
@@ -3,6 +3,6 @@
 BINDGEN=../rust-bindgen/target/debug/bindgen
 LIBNFC=/usr/include/nfc
 
-$BINDGEN -l$LIBNFC -match nfc -allow-unknown-types -o src/nfc-emulation.rs $LIBNFC/nfc-emulation.h
-$BINDGEN -l$LIBNFC -match nfc -allow-unknown-types -o src/nfc-types.rs $LIBNFC/nfc-types.h
-$BINDGEN -l$LIBNFC -match nfc -allow-unknown-types -o src/nfc.rs $LIBNFC/nfc.h
+$BINDGEN -l$LIBNFC -match nfc -allow-unknown-types -o src/nfc.rs $LIBNFC/nfc.h \
+         -I/usr/include/nfc/nfc-emulation.h \
+         -I/usr/include/nfc/nfc-types.h
